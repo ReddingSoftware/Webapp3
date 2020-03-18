7fe3d07626da55e214ffc1544732efd8730ab382
Redding Software <61996426+ReddingSoftware@users.noreply.github.com>
2020-03-18 09:38:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #This file is just an example file to demonstrate how the webapp3-flask module works
 
 from flask import Flask, session,request,redirect
+"""
+If you want to use the mail feature to convert the google mail syntax to work for mailgun you need to either 
+add this class before you import webapp3 or put this class in a python file and name it MailConfigFile.py and import the file 
+BEFORE you import webapp3
+class MailConfig():
+    config={"Key":"your-Mailgun-Key","Defalt_Sender":"YOU@YOUR_DOMAIN_NAME","Always_Use_Defalt_Sender":True,"URL":"https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages","First_Name":"Default First Name","Last_Name":"Default Last Name"}
+"""
+from MailConfigFile import MailConfig
 from webapp3 import *
 #or you can use
 #from webapp3 import BaseHandler,webapp2,RequestHandler,webappGet,webappPost
